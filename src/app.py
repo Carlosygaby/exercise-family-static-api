516a4a9cbc67219d65aa52b94e6274afce424912
Carlos <191144185+Carlosygaby@users.noreply.github.com>
2025-04-10 16:08:42 +0000

    Pending changes exported from your codespace

--- src/app.py
+++ src/app.py
@@ -25,6 +25,8 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# Obtener todos los miembros 
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
@@ -35,8 +37,55 @@ def handle_hello():
         "family": members
     }
 
-
     return jsonify(response_body), 200
+
+# Obtener un miembro en especifico
+
+@app.route("/members/<int:id>",methods=["GET"])
+def member_view(id):
+    miembro = jackson_family.get_member(id)
+    if miembro == "":
+        return jsonify(message= "recuso no encontrado"),404
+    else:
+        response_body ={
+            "id": miembro["id"],
+            "firts_name": miembro["name"],
+            "last_name": miembro["last_name"],
+            "age": miembro["age"]
+        }   
+        return jsonify(response_body),200
+    
+# Eliminar un miembro en especifico
+
+@app.route("/members/<int:id>", methods=["DELETE"])
+def delete_member(id):
+    delete = jackson_family.delete_member(id)
+    if delete == "":
+        response_body={
+            "done":False
+        }
+        return jsonify(response_body),404
+    else:
+        response_body={
+        "done":True
+        }
+        return jsonify(response_body),200
+    
+# Ruta para postear un miembro
+    
+@app.route("/members", methods=["POST"])
+def agregar_miembro():
+    member= request.get_json()
+    agregar = jackson_family.add_member(member)
+    if agregar == "":
+        return jsonify(message= "Falta algun atributo"),400
+    else:
+       response_body ={
+           "done":True
+       }
+       return jsonify(response_body),200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,23 +13,50 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            {"name":"John","last_name":"Jackson","age":33,"lucky_numbers":[7,13,22],"id":1},
+            {"name":"Jane","last_name":"Jackson","age":35,"lucky_numbers":[10.14,3],"id":2},
+            {"name":"Jimmy","last_name":"Jackson","age":5,"lucky_numbers":[5],"id":3}
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        name = member.get("first_name")
+        age = member.get("age")
+        lucky_numbers = member.get("lucky_numbers")
+        
+
+        if not name or not age or not lucky_numbers:
+            return ""
+        else:
+            self._members.append({
+                "name":name,
+                "last_name":"Jackson",
+                "age":age,
+                "lucky_numbers":lucky_numbers,
+                "id": self._generateId()
+            })
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                self._members.remove(member)
+                return
+            
+        return ""
+        
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if id == member["id"]:
+                return member
+            
+        return ""
+                
+        
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
